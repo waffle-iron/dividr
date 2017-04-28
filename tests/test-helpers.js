696032669bc4f9412266527924015c3c4bd6c84e
Chris Gray <cmg_business@hotmail.com>
2017-04-28 09:24:17 +0100

    Commented createLoginToken function

--- tests/test-helpers.js
+++ tests/test-helpers.js
@@ -1,16 +1,22 @@
 const request = require('supertest');
 
-// Auxiliary function.
+// Auxiliary function. Takes server from express, a valid login user and returns a loginToken and userId through the done callback
 function createLoginToken(server, loginDetails, done) {
     request(server)
+        // POST request to /login
         .post('/login')
+        // Send valid user loginDetails
         .send(loginDetails)
         .end(function(error, response) {
+            // If error thrown return that
             if (error) {
                 throw error;
             }
+            // Else set variable to value of res.body token
             let loginToken = response.body.token;
+            // Else set variable to value of res.body user ID
             let userId = response.body.user._id;
+            // Return above variables for use in requests
             done(loginToken, userId);
         });
 }
