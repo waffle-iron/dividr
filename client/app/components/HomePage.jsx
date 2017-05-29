e8318798f9d3009dc472a9d9a0df0f0012143069
Chris Gray <cmg_business@hotmail.com>
2017-05-29 12:20:18 +0100

    Add copy first edition

--- client/app/components/HomePage.jsx
+++ client/app/components/HomePage.jsx
@@ -13,68 +13,62 @@ class HomePage extends React.Component {
                     </div>
                 </div>
                 <div className="marketing-site-features">
-                    <h2 className="marketing-site-features-headline">Yeti Approved Features</h2>
-                    <p className="marketing-site-features-subheadline subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit quaerat eos words and phrases.</p>
+                    <h2 className="marketing-site-features-headline">Features:</h2>
                     <div className="row">
-                        <div className="small-12 medium-3 columns">
-                            <i className="fa fa-snowflake-o" aria-hidden="true" />
-                            <h4 className="marketing-site-features-title">Special Snowflake</h4>
-                            <p className="marketing-site-features-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <div className="small-12 medium-4 columns text-center">
+                            <i className="fa fa-clock-o" aria-hidden="true" />
+                            <h4 className="marketing-site-features-title">Quick Setup</h4>
+                            <p className="marketing-site-features-desc">Get an account and your first weeks meals set up in less than 10 minutes</p>
                         </div>
-                        <div className="small-12 medium-3 columns">
-                            <i className="fa fa-angellist" aria-hidden="true" />
-                            <h4 className="marketing-site-features-title">Friendly Forms </h4>
-                            <p className="marketing-site-features-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <div className="small-12 medium-4 columns text-center">
+                            <i className="fa fa-check" aria-hidden="true" />
+                            <h4 className="marketing-site-features-title">Easy to use </h4>
+                            <p className="marketing-site-features-desc">An intuitive interface makes it simple to use and work with</p>
                         </div>
-                        <div className="small-12 medium-3 columns">
-                            <i className="fa fa-bullseye" aria-hidden="true" />
-                            <h4 className="marketing-site-features-title">Special Snowflake</h4>
-                            <p className="marketing-site-features-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                        </div>
-                        <div className="small-12 medium-3 columns">
-                            <i className="fa fa-battery-full" aria-hidden="true" />
-                            <h4 className="marketing-site-features-title">Lithium Ion</h4>
-                            <p className="marketing-site-features-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <div className="small-12 medium-4 columns text-center">
+                            <i className="fa fa-money" aria-hidden="true" />
+                            <h4 className="marketing-site-features-title">Free to use</h4>
+                            <p className="marketing-site-features-desc">Dividr will always be free to use indefinitely, so no extra cost</p>
                         </div>
                     </div>
                 </div>
                 <div className="marketing-site-content-section">
                     <div className="marketing-site-content-section-img">
-                        <img src="https://images.pexels.com/photos/256046/pexels-photo-256046.jpeg?h=350&auto=compress&cs=tinysrgb" alt="" />
+                        <img src="https://images.unsplash.com/photo-1471367029819-38078e88c3fb?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=" alt="" />
                     </div>
                     <div className="marketing-site-content-section-block">
-                        <h3 className="marketing-site-content-section-block-header">Yeti Snowcone Agency</h3>
-                        <p className="marketing-site-content-section-block-subheader subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam omnis, maxime libero natus qui minus!</p>
-                        <a href="#" className="round button small">learn more</a>
+                        <h3 className="marketing-site-content-section-block-header">Make Food Prep Easy</h3>
+                        <p className="marketing-site-content-section-block-subheader subheader">No more eyeballing portions and coming up short, this works out the exact amount per portion size</p>
                     </div>
                     <div className="marketing-site-content-section-block small-order-2 medium-order-1">
-                        <h3 className="marketing-site-content-section-block-header">Yeti Snowcone Agency</h3>
-                        <p className="marketing-site-content-section-block-subheader subheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam omnis, maxime libero natus qui minus!</p>
-                        <a href="#" className="round button small">learn more</a>
+                        <h3 className="marketing-site-content-section-block-header">Works on mobile</h3>
+                        <p className="marketing-site-content-section-block-subheader subheader">No need to memorise measurements, the app works on mobile just as well as desktop</p>
                     </div>
                     <div className="marketing-site-content-section-img small-order-1 medium-order-2">
-                        <img src="https://images.pexels.com/photos/300857/pexels-photo-300857.jpeg?h=350&auto=compress&cs=tinysrgb" alt="" />
+                        <img src="https://images.unsplash.com/photo-1430163393927-3dab9af7ea38?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=" alt="" />
                     </div>
                 </div>
                 <footer className="marketing-site-footer">
                     <div className="row medium-unstack">
                         <div className="medium-4 columns">
-                            <h4 className="marketing-site-footer-name">Yeti Snowcone</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita dolorem accusantium architecto id quidem, itaque nesciunt quam ducimus atque.</p>
-                            <ul className="menu marketing-site-footer-menu-social simple">
-                                <li><a href="#"><i className="fa fa-youtube-square" aria-hidden="true" /></a></li>
-                                <li><a href="#"><i className="fa fa-facebook-square" aria-hidden="true" /></a></li>
-                                <li><a href="#"><i className="fa fa-twitter-square" aria-hidden="true"/></a></li>
-                            </ul>
+                            <h4 className="marketing-site-footer-name">Dividr</h4>
+                            <p>Quick app to portion up meal sizes, <br/>
+                                Any questions or improvements? Get in touch on <a href="mailto:cgray@hourglassdevelopment.co.uk">cgray@hourglassdevelopment.co.uk</a></p>
                         </div>
                     </div>
                     <div className="marketing-site-footer-bottom">
                         <div className="row large-unstack">
                             <div className="column">
                                 <ul className="menu marketing-site-footer-bottom-links">
-                                    <li><a href="#">Home</a></li>
-                                    <li><a href="#">News</a></li>
-                                    <li><a href="#">Contact</a></li>
+                                    <li>
+                                        <Link to="/">Home</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/news">News</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/contact">Contact</Link>
+                                    </li>
                                 </ul>
                             </div>
                             <div id="copyright" className="column float-right">
